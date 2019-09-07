1b6967b94c00e7c25b5c456a7733234009bd4bee
candra saputra <candrasaputra@live.com>
2019-09-08 04:30:42 +0700

    logout

--- client/public/js/signin.js
+++ client/public/js/signin.js
@@ -1,16 +1,24 @@
 (() => {
     const baseUrl = "http://localhost:3000";
-
     $("#signin").bind("click", function () {
         $.ajax({
             type: "POST",
             url: baseUrl + '/signin',
             data: $("#frmSignIn").serialize(),
             success: function (response) {
-                console.log(response);
+                localStorage.setItem('token', response.token);
+                window.location.href = "#todos-active"
             },
             error: function (jqXHR, textStatus, errorThrown) {
-                console.log(textStatus, errorThrown);
+
+                let data = jqXHR.responseJSON;
+                Swal.fire({
+                    position: 'top-end',
+                    type: 'error',
+                    title: data.message,
+                    showConfirmButton: false,
+                    timer: 1500
+                })
             }
         });
     });
