f257e15267d7b924fca0e555930bfaa4f99d99aa
candra saputra <candrasaputra@live.com>
2019-09-08 23:42:52 +0700

    init project page

--- client/public/js/todos.js
+++ client/public/js/todos.js
@@ -1,30 +1,3 @@
-var baseUrl = "http://localhost:3000";
-
-(() => {
-
-    function displayName() {
-        $('#displayName').text(localStorage.getItem('name'));
-    }
-
-    displayName()
-
-    function isSignin() {
-        if (!localStorage.getItem('token')) {
-            window.location.href = "#signin";
-
-            Swal.fire({
-                position: 'top-center',
-                type: 'error',
-                title: "you does not have permission to access this page.",
-                showConfirmButton: false,
-                timer: 1500
-            })
-        }
-    }
-
-    isSignin();
-})();
-
 function formatDate(date) {
     let monthNames = ['',
         "January", "February", "March",
