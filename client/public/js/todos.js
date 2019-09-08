699b8e2ebcbb059845839a1e020ba1eebd3bb38d
candra saputra <candrasaputra@live.com>
2019-09-08 12:12:28 +0700

    add todo

--- client/public/js/todos.js
+++ client/public/js/todos.js
@@ -1,4 +1,6 @@
 (() => {
+    const baseUrl = "http://localhost:3000";
+
     function displayName() {
         $('#displayName').text(localStorage.getItem('name'));
     }
@@ -20,4 +22,44 @@
     }
 
     isSignin();
+
+    $("#addTaskButton").bind("click", function () {
+        $('#modal-task').modal('show');
+        $('#modal-task .block-title').text('Add a new task');
+        $('#modal-task form')[0].reset();
+    });
+
+    $("#saveTaskButton").bind("click", function () {
+        $('#modal-task').modal('hide');
+        Swal.showLoading();
+
+        $.ajax({
+            type: "POST",
+            url: baseUrl + '/todos',
+            data: $("#modal-task form").serialize(),
+            beforeSend: function (request) {
+                request.setRequestHeader("token", localStorage.getItem('token'));
+            },
+            success: function (response) {
+                Swal.fire({
+                    position: 'top-center',
+                    type: 'success',
+                    title: response.message,
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                let data = jqXHR.responseJSON;
+                Swal.close();
+
+                $('#modal-task').modal('show');
+                Swal.fire({
+                    type: 'error',
+                    title: 'Oops...',
+                    text: data.message
+                });
+            }
+        });
+    });
 })();
