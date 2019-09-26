9765deb5430858430ea09caca2861cf464a3171c
saveroz <muhammadsavero@gmail.com>
2019-09-27 03:31:11 +0700

    buat cron.js

--- server/routes/TodoRoutes.js
+++ server/routes/TodoRoutes.js
@@ -3,13 +3,9 @@ const router = express.Router()
 const TodoController = require('../controllers/TodoController')
 const authentication = require('../middleware/authentication')
 const { authorizationTodo } = require('../middleware/authorization')
-const sendIncompletedTodo = require("../helpers/incompletedTodo")
-const CronJob = require("cron").CronJob
-
 
 router.use(authentication)
 
-// router.get('/extra/uncompleted', UncompletedTodo)
 router.get('/', TodoController.getAll)
 router.get('/projects/:id', TodoController.getByProject)
 router.get('/:id', TodoController.getOne)
@@ -17,11 +13,5 @@ router.post('/', TodoController.create)
 router.patch('/:id', authorizationTodo, TodoController.update)
 router.delete('/:id', authorizationTodo, TodoController.delete)
 
-
-new CronJob(' 0 8 * * 1', function () {
-
-    sendIncompletedTodo()
-
-}, null, true, 'Asia/Jakarta');
 
 module.exports = router
